59ce3628c94e11b6a8d8fe243698b7ef242f06d1
Maurice <mrcgrhrdt@gmail.com>
2018-10-24 21:48:41 +0200

    update

--- Example/Color2UiColor/ViewController.swift
+++ Example/Color2UiColor/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import Color2UiColor
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +23,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 }
 
